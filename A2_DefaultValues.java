2423cb46f0163cf8fb43eed100646081cb733010
sushanksharma <sushanks07@gmail.com>
2021-03-29 22:24:02 +0530

    remove *.java

--- A2_DefaultValues.java
+++
@@ -1,9 +0,0 @@
-class A2_DefaultValues{
-	public static void main(String args[]){
-		System.out.println("int : "+ (Integer.SIZE/8));
-		System.out.println("char : "+ (Character.SIZE/8));
-		System.out.println("Long : "+ (Long.SIZE/8));
-		System.out.println("double : "+ (Double.SIZE/8));
-		System.out.println("float : "+ (Float.SIZE/8));
-	}
-}
